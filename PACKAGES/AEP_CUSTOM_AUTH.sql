72adc52d82919441a0cc2f18b52dc2c72561be23
Grima-2024 <grima@oracleapexconsultant.com>
2024-03-07 17:28:24 +0530

    Grima_002

--- PACKAGES/AEP_CUSTOM_AUTH.sql
+++ PACKAGES/AEP_CUSTOM_AUTH.sql
@@ -2,13 +2,12 @@
 --  DDL for Package AEP_CUSTOM_AUTH
 --------------------------------------------------------
 
-  CREATE OR REPLACE EDITIONABLE PACKAGE "INSTITUTE"."AEP_CUSTOM_AUTH" AS  
+CREATE OR REPLACE PACKAGE "AEP_CUSTOM_AUTH" AS  
  
 	FUNCTION AEP_AUTH_RIGHT(p_app_user IN VARCHAR, p_usertype IN VARCHAR2)   
-	RETURN BOOLEAN; 
+	RETURN BOOLEAN;  
 
-	--New line added
- 
 END AEP_CUSTOM_AUTH; 
+--HELLO WORLD
 
 /
--- PACKAGES/AEP_CUSTOM_LOGIN.sql
+++ PACKAGES/AEP_CUSTOM_LOGIN.sql
@@ -2,13 +2,14 @@
 --  DDL for Package AEP_CUSTOM_LOGIN
 --------------------------------------------------------
 
-  CREATE OR REPLACE EDITIONABLE PACKAGE "INSTITUTE"."AEP_CUSTOM_LOGIN" AS   
+CREATE OR REPLACE PACKAGE "AEP_CUSTOM_LOGIN" AS   
  
 	FUNCTION AEP_login ( 
 		p_username IN VARCHAR2,  
 		p_password IN VARCHAR2)   
 	RETURN BOOLEAN;  
- 
+	
+	--NEW ADDED
 END AEP_CUSTOM_LOGIN;
 
 /
